d8d98f80c9f6a4ec855cef16dd708473944daf25
Ange-Gaëlle <ange.batie@gmail.com>
2026-09-22 17:45:10 -0500

    All tests passing

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.querySelector("#create-task-form");
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const task = e.target.elements["new-task-description"].value;
+
+    buildToDo(task);
+  });
+
+  function buildToDo(task) {
+    const li = document.createElement("li");
+    li.textContent = task;
+
+    const taskList = document.querySelector("#tasks");
+    taskList.append(li);
+  };
 });
